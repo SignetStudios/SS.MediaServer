7b35f4944868855cb91bb3f9d1a402d3e6a40c90
James McCollum <jmccollum@signet-studios.com>
2023-09-30 08:45:36 -0500

    Add irssi scripts

---
+++ scripts/irssi/irssi_radarr.py
@@ -0,0 +1,19 @@
+import argparse
+import configparser
+import logging
+import os
+from irssi_general import gen_payload, send_pvr
+
+filePath = os.path.dirname(__file__)
+logging.basicConfig(filename='{0}/irssi.log'.format(filePath),level=logging.DEBUG)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--title", required=True)
+    parser.add_argument("--downloadUrl", required=True)
+    parser.add_argument("--tracker")
+    parser.add_argument("--torrentSize")
+
+    args = parser.parse_args()
+
+    send_pvr('radarr', args.title, args.downloadUrl, tracker=args.tracker, fileSize=args.torrentSize)
