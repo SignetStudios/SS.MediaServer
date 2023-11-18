7e6951798720cf13f205a545a4fe02187a29d577
James McCollum <jmccollum@signet-studios.com>
2023-11-18 13:41:36 -0600

    Inject cross-seed connection info

--- scripts/sendToCrossSeed_Radarr.sh
+++ scripts/sendToCrossSeed_Radarr.sh
@@ -5,7 +5,7 @@ if [[ ${radarr_eventtype^^} == "TEST" ]]; then
 	release_hash="Test Movie Release"
 fi
 
-curl -XPOST ${CROSS_SEED_HOST}/api/webhook \
+curl -XPOST ${CROSS_SEED_HOST}/api/webhook?apikey=${CROSS_SEED_API} \
   -H 'Content-Type: application/json' \
   --data '{"infoHash":"'"${release_hash}"'"}'
 
--- scripts/sendToCrossSeed_Sonarr.sh
+++ scripts/sendToCrossSeed_Sonarr.sh
@@ -5,7 +5,7 @@ if [[ ${sonarr_eventtype^^} == "TEST" ]]; then
 	release_hash="Test TV Release"
 fi
 
-curl -XPOST ${CROSS_SEED_HOST}/api/webhook \
+curl -XPOST ${CROSS_SEED_HOST}/api/webhook?apikey=${CROSS_SEED_API} \
   -H 'Content-Type: application/json' \
   --data '{"infoHash":"'"${release_hash}"'"}'
 
